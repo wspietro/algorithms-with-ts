6163e900c96a2fcb2b26ce877c322ad058aa3f93
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-03-26 12:31:06 -0300

    two-crystal-ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,25 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// we want to jump by sqrt(n)
+// sqrt(n) is the only way we can change this from nonlinear running
+// using sqrt(n) means that in the second ball, the worst case scenario is to walk sqrt(n)
+// we use our first ball to see where it wil break and then we jump back sqrt(n) and walk forward sqrt(n) to find the exact point
+// find the point where it breaks, walk back sqrt(n) and then walk foward sqrt(n)
 
+
+export default function two_crystal_balls(breaks: boolean[]): number {
+  const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+  let i = jmpAmount
+  for (; i < breaks.length; i += jmpAmount) {
+    if (breaks[i]) { // find where the crystall ball is going to break
+      breaks;
+    }
+  }
+
+  i -= jmpAmount; // jumped back sqrt(n) after finding a point where breaks its already true
+  for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i)// walk foward
+    if (breaks[i]) {
+      return i;
+    }
+
+  return -1
 }

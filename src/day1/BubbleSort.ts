caea80c45286385fc21f3ba7160973e4b768c0c5
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-11 21:47:10 -0300

    bubble-sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -3,9 +3,8 @@
 // bubles the element tho its correct position(last). Then we dont need to campare the last one in the next loop
 
 export default function bubble_sort(arr: number[]): void {
-  for (let i = 0; i < arr.length; ++i) {
-    // up too but not include also the last element to the first iteration cuz we r gonna do a plus 1.  The last element becomes the largest, we dont need to redo that. thas why we have minus i
-    for (let j = 0; j < arr.length - 1 - i; ++j) {
+  for (let i = 0; i < arr.length; ++i) { // ultimo elemento sempre estará ordenado.
+    for (let j = 0; j < arr.length - 1 - i; ++j) {// up too but not include also the last element to the first iteration cuz we r gonna do a plus 1.  The last element becomes the largest, we dont need to redo that. thas why we have minus i
       if (arr[j] > arr[j + 1]) {
         const tmp = arr[j]
         arr[j] = arr[j + 1];

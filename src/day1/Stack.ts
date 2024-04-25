9d50fdfa1bb889ff0271dbe26d5116775191f884
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-25 10:18:57 -0300

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+  value: T,
+  prev?: Node<T>, // visualizar de maneira mais fácio o fluxo
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0
+    this.head = undefined
+  }
 
-    constructor() {
+  // adds
+  push(item: T): void {
+    const node: Node<T> = { value: item, prev: this.head }
+    this.length++
+
+    this.head = node
+  }
+
+  // removes and returns
+  pop(): T | undefined {
+    if (!this.head) {
+      return undefined
     }
 
-    push(item: T): void {
+    this.length--;
 
-}
-    pop(): T | undefined {
 
-}
-    peek(): T | undefined {
+    const head = this.head
+    this.head = head.prev
+    head.prev = undefined
 
-}
+    return head.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }

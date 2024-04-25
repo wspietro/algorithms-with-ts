48183f15a6c1ef705fb8569c772ee9b5fb36f134
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-04-25 10:18:48 -0300

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,57 @@
+type Node<T> = {
+  value: T
+  next?: Node<T>
+}
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0
+    this.head = this.tail = undefined
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node: Node<T> = { value: item }
+
+    this.length++;
+    // array vazio
+    if (!this.tail) {
+      this.tail = this.head = node
+      return
     }
 
-    enqueue(item: T): void {
+    // Cuidado para não perder referencia
+    this.tail.next = node
+    this.tail = node
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    // Save your head
+    // update your head
+    // return your head
+    const head = this.head
+    this.head = this.head.next
+
+    //free
+    head.next = undefined
+
+    if (this.length === 0) {
+      this.tail = undefined
+    }
+
+    return head.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
+
 }

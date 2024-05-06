d71d41e33bda37af4f4d321961058d468c9b777c
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-05-06 15:06:26 -0300

    data structure: array-list

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,85 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number;
+  private data: T[];
 
-    
+  private growCapacity(): void {
+    // memcopy? how to implement in js? .subarray?
+    if (this.length === this.data.length) {
+      const prevArray = this.data
+      this.data = new Array(this.data.length * 2); // novo array com n posições vazias, precisamos copiar array anterior
+      for (let i = 0; i < prevArray.length; i++) {
+        this.data[i] = prevArray[i]
+      }
+    }
+  }
 
-    constructor() {
+
+  constructor(capacity: number = 5) { // capacidade inicial
+    this.data = Array(capacity) // array com 5 posições vazias, this.data.lenght = 5 = capacity
+    this.length = 0
+  }
+
+  prepend(item: T): void {
+    this.insertAt(item, 0);
+  }
+
+  insertAt(item: T, idx: number): void {
+    this.growCapacity();
+
+    // começar do último ítem
+    // mover os elementos até que o idx seja movido também (por isso >=)
+    // mover = shift, i+1 = i
+    // a cada loop, "descer" um elemento
+    for (let i = this.length - 1; i >= idx; --i) {
+      this.data[i + 1] = this.data[i]
     }
 
-    prepend(item: T): void {
+    this.length++;
+    this.data[idx] = item
+  }
 
-}
-    insertAt(item: T, idx: number): void {
+  append(item: T): void {
+    this.growCapacity();
+    this.data[this.length] = item;
+    this.length++;
+  }
 
-}
-    append(item: T): void {
 
-}
-    remove(item: T): T | undefined {
+  remove(item: T): T | undefined {
+    let itemIndex = undefined;
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === item) {
+        itemIndex = i
+        break
+      }
+    }
 
-}
-    get(idx: number): T | undefined {
+    if (itemIndex === undefined) { // 0 é falsy
+      return undefined
+    }
 
-}
-    removeAt(idx: number): T | undefined {
+    return this.removeAt(itemIndex)
+  }
 
-}
+  get(idx: number): T | undefined {
+    if (idx >= this.length) {
+      return undefined
+    }
+
+    return this.data[idx]
+  }
+
+  removeAt(idx: number): T | undefined {
+    if (idx >= this.length) {
+      return undefined
+    }
+
+    let elemetToRemove = this.data[idx];
+    this.length--;
+
+    for (let i = idx; i < this.length; i++) { // iniciando do elemento a remover, fazemos o shift para a esqueda da lista.
+      this.data[i] = this.data[i + 1]
+    }
+    return elemetToRemove
+  }
 }
--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,8 +1,8 @@
 // Media, Low, High, Value, N input
-// m = [lo + (hi - lo) / 2 ] -> evita estouro de inteiros 
+// m = [lo + (hi - lo) / 2 ] -> not overflow in languages with fixed sized integers (bits).
 // v = arr[m]
 // if v = n return else if v > m lo = m + 1 else hi = m
-// [hi, lo). inclusive and exclusive
+// [lo, hi). inclusive and exclusive
 
 export default function bs_list(haystack: number[], needle: number): boolean {
   let lo = 0;
@@ -15,9 +15,9 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     if (v === needle) {
       return true
     } else if (v > needle) {
-      hi = m // include
+      hi = m
     } else {
-      lo = m + 1 // exclude
+      lo = m + 1
     }
   } while (lo < hi);
 
